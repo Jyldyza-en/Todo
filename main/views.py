ee15af2f299575c284bd65ab37c3710d6917c5f8
Jyldyza-en <sultanbekova.jyldyz@gmail.com>
2021-01-23 18:49:51 +0600

    in books.html added a form for adding new books in the browser without the admin panel

--- main/views.py
+++ main/views.py
@@ -30,3 +30,16 @@ def add_todo(request):
     todo = ToDo(text=text)
     todo.save()
     return redirect(test)
+
+def add_book(request):
+    form = request.POST
+    title = form["books_title"]
+    subtitle = form["books_subtitle"]
+    description = form["books_description"]
+    price = form["books_price"]
+    genre = form["books_genre"]
+    author = form["books_author"]
+    year = form["books_year"]
+    book = Bookstore(title=title,subtitle=subtitle,description=description,price=price,genre=genre,author=author,year=year)
+    book.save()
+    return redirect(second)                
